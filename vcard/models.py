cf4d0be503da28e5081f4f147642b78fd7957cf3
Komilxon <komilkhon.asamov@hotmail.com>
2016-06-14 17:28:16 +0500

    Add __str__ method to models

--- vcard/models.py
+++ vcard/models.py
@@ -1,15 +1,12 @@
 from django.db import models
 
 
-
 class ProjectTag(models.Model):
     caption = models.CharField(max_length=255)
     alias = models.CharField(max_length=255, unique=True)
 
-
     def __str__(self):
         return self.caption
-
 
 
 class Project(models.Model):
@@ -17,7 +14,12 @@ class Project(models.Model):
     caption = models.CharField(max_length=255)
     tag = models.ManyToManyField(ProjectTag)
 
+    def __str__(self):
+        return self.caption
 
 
 class ContactInfo(models.Model):
     text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
